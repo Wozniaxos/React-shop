010db209e924971547127e78188f82e94fff756c
Wozniaxos <kacperwozniax@gmail.com>
2018-06-19 23:08:51 +0200

    Moved session logic to higher order component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from './Navigation';
@@ -8,69 +8,50 @@ import SignInPage from './SignIn';
 import PasswordForgetPage from './PasswordForget';
 import HomePage from './Home';
 import AccountPage from './Account';
-import auth from '../firebase/firebase';
+import withAuthentication from './withAuthentication';
 
 import * as routes from '../constants/routes';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () =>
+  (
+    <Router>
+      <div>
+        <Navigation />
 
-    this.state = {
-      authUser: null,
-    };
-  }
+        <hr />
 
-  componentDidMount() {
-    auth.onAuthStateChanged((authUser) => {
-      authUser
-        ? this.setState(() => ({ authUser }))
-        : this.setState(() => ({ authUser: null }));
-    });
-  }
+        <Route
+          exact
+          path={routes.LANDING}
+          component={() => <LandingPage />}
+        />
+        <Route
+          exact
+          path={routes.SIGN_UP}
+          component={() => <SignUpPage />}
+        />
+        <Route
+          exact
+          path={routes.SIGN_IN}
+          component={() => <SignInPage />}
+        />
+        <Route
+          exact
+          path={routes.PASSWORD_FORGET}
+          component={() => <PasswordForgetPage />}
+        />
+        <Route
+          exact
+          path={routes.HOME}
+          component={() => <HomePage />}
+        />
+        <Route
+          exact
+          path={routes.ACCOUNT}
+          component={() => <AccountPage />}
+        />
+      </div>
+    </Router>
+  );
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navigation authUser={this.state.authUser} />
-
-          <hr />
-
-          <Route
-            exact
-            path={routes.LANDING}
-            component={() => <LandingPage />}
-          />
-          <Route
-            exact
-            path={routes.SIGN_UP}
-            component={() => <SignUpPage />}
-          />
-          <Route
-            exact
-            path={routes.SIGN_IN}
-            component={() => <SignInPage />}
-          />
-          <Route
-            exact
-            path={routes.PASSWORD_FORGET}
-            component={() => <PasswordForgetPage />}
-          />
-          <Route
-            exact
-            path={routes.HOME}
-            component={() => <HomePage />}
-          />
-          <Route
-            exact
-            path={routes.ACCOUNT}
-            component={() => <AccountPage />}
-          />
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default App;
+export default withAuthentication(App);
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignOutButton from './SignOut';
+import AuthUserContext from './AuthUserContext';
 
 import * as routes from '../constants/routes';
 
-const Navigation = ({ authUser }) =>
+const Navigation = () =>
   (
-    <div>
-      { authUser
+    <AuthUserContext.Consumer>
+      {authUser => (authUser
         ? <NavigationAuth />
         : <NavigationNonAuth />
-      }
-    </div>
+      )}
+    </AuthUserContext.Consumer>
   );
 
 const NavigationAuth = () =>
