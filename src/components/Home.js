a3e5297c97f23bd827e4b6a9732aefed0bf9e238
Wozniaxos <kacperwozniax@gmail.com>
2018-06-20 16:26:11 +0200

    Some refactor.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import withAuthorization from './withAuthorization'
 import { db } from '../firebase'
+import UserList from './UserList'
 
 class HomePage extends Component {
   state = { users: null }
@@ -23,24 +23,6 @@ class HomePage extends Component {
       </div>
     )
   }
-}
-
-const UserList = ({ users }) => (
-  <div>
-    <h2>List of Usernames of Users</h2>
-    <p>(Saved on Sign Up in Firebase Database)</p>
-
-    {Object.keys(users).map(key => <div key={key}>{users[key].username}</div>)}
-  </div>
-)
-
-UserList.propTypes = {
-  users: PropTypes.objectOf(
-    PropTypes.shape({
-      email: PropTypes.string.isRequired,
-      username: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
 }
 
 const authCondition = authUser => !!authUser
---
+++ src/components/UserList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserList = ({ users }) =>
+  (
+    <div>
+      <h2>List of Usernames of Users</h2>
+      <p>(Saved on Sign Up in Firebase Database)</p>
+
+      {Object.keys(users).map(key =>
+        <div key={key}>{users[key].username}</div>)}
+    </div>
+  );
+
+UserList.propTypes = {
+  users: PropTypes.objectOf(PropTypes.shape({
+    email: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default UserList;
