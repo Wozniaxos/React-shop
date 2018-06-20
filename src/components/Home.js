f8c72de39b5f729dedd5ddd617d510bdfe0d0605
Wozniaxos <kacperwozniax@gmail.com>
2018-06-20 10:18:13 +0200

    Scured home and account routes and gave possibility to manage auth conditions.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const LandingPage = () => (
+import withAuthorization from './withAuthorization'
+
+const HomePage = () => (
   <div>
     <h1>Home Page</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
   </div>
 )
 
-export default LandingPage
+const authCondition = authUser => !!authUser
+
+export default withAuthorization(authCondition)(HomePage)
