18bdd86665bfa6ffe6a2c3bb1e12a8708bf4be98
Wozniaxos <kacperwozniax@gmail.com>
2018-06-25 15:21:24 +0200

    Added main order panel component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import withAuthorization from './withAuthorization'
+import OrderPanel from './OrderPanel'
 
 class HomePage extends Component {
   render() {
@@ -7,6 +8,7 @@ class HomePage extends Component {
       <div>
         <h1>Home</h1>
         <p>Here will be the store with side bars etc</p>
+        <OrderPanel />
       </div>
     )
   }
---
+++ src/components/OrderPanel.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import OrderList from './OrderList'
+import OrderSideBar from './OrderSideBar'
+import { setProducts } from '../store/products/actions'
+import { setOrderItems } from '../store/order/actions'
+import { connect } from 'react-redux'
+import { db } from '../firebase'
+
+class OrderPanel extends Component {
+  componentDidMount() {
+    const { onSetProducts, onSetOrderItems } = this.props
+    db.fetchAndHandleChangesFor('Product', snapshot => onSetProducts(snapshot.val() || {}))
+    db.fetchAndHandleChangesFor('Order', snapshot => onSetOrderItems(snapshot.val() || {}))
+  }
+
+  render() {
+    const { productList, orderItemsList, products, orderItems } = this.props
+
+    return (
+      <div>
+        <div>
+          <h1>Products</h1>
+          {productList.length > 0 && <OrderList list={productList} products={products} />}
+        </div>
+        <div>
+          <OrderSideBar items={orderItems} itemsList={orderItemsList} />
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  productList: state.products.list,
+  products: state.products.byId,
+  orderItemsList: state.order.list,
+  orderItems: state.order.byId,
+})
+
+const mapDispatchToProps = {
+  onSetProducts: setProducts,
+  onSetOrderItems: setOrderItems,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderPanel)
